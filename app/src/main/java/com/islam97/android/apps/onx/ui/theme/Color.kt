f8a42a02cbab946dba443b14d34a06acbed24c80
Islam Hani <72826869+islamhani97@users.noreply.github.com>
2025-05-09 01:56:30 +0300

    Set up theme and color scheme

--- app/src/main/java/com/islam97/android/apps/onx/ui/theme/Color.kt
+++ app/src/main/java/com/islam97/android/apps/onx/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.islam97.android.apps.onx.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White: Color = Color.White
+val Black: Color = Color.Black
+val Transparent: Color = Color.Transparent
+val Primary = Color(0xFF004F62)
+val SecondaryLight = Color(0xFFCBFFCB)
+val Secondary = Color(0xFF29D40F)
+val SecondaryDark = Color(0xFF39A238)
+val TertiaryExtremeLight = Color(0xFFF1F5FB)
+val TertiaryExtraLight = Color(0xFFE9FAFF)
+val Tertiary = Color(0xFF0A3F8F)
+val Error = Color(0xFFD42A0F)
+val NeutralLight = Color(0xFFF4F4F4)
+val Neutral = Color(0xFFC7C7C7)
+val NeutralDark = Color(0xFF808080)
+val NeutralExtraDark = Color(0xFF707070)
