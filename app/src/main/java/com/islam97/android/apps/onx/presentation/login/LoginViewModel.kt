8aa5d8a4ab5a5d2f669b4c19d5681223c84bec5a
Islam Hani <72826869+islamhani97@users.noreply.github.com>
2025-05-09 17:57:30 +0300

    Add splash screen (#3)

---
+++ app/src/main/java/com/islam97/android/apps/onx/presentation/login/LoginViewModel.kt
@@ -0,0 +1,11 @@
+package com.islam97.android.apps.onx.presentation.login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel
+@Inject constructor() : ViewModel() {
+
+}
---
+++ app/src/main/java/com/islam97/android/apps/onx/presentation/main/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.islam97.android.apps.onx.presentation.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.islam97.android.apps.onx.presentation.login.LoginScreen
+import com.islam97.android.apps.onx.presentation.login.RouteLoginScreen
+import com.islam97.android.apps.onx.presentation.splash.RouteSplashScreen
+import com.islam97.android.apps.onx.presentation.splash.SplashScreen
+import com.islam97.android.apps.onx.presentation.ui.theme.ONXTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { ONXTheme { MainNavigation() } }
+    }
+}
+
+@Composable
+fun MainNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = RouteSplashScreen) {
+        composable<RouteSplashScreen> {
+            SplashScreen(navController)
+        }
+
+        composable<RouteLoginScreen> {
+            LoginScreen(navController)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainActivity() {
+    ONXTheme { MainNavigation() }
+}
